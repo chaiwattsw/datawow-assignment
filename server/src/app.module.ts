cb1bf23902bb8532291b3e21a5cfe37a1e1a901e
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2024-09-22 23:40:46 +0700

    chore: add bcrypt to server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConcertsModule } from './concerts/concerts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ConcertsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
