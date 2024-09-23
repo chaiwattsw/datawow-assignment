d452c4361cb898e63aa6f43171307790005220a5
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2024-09-23 23:30:05 +0700

    feat: client middleware

---
+++ client/middleware.ts
@@ -0,0 +1,31 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    // If the user is not logged in, redirect to the login page
+    if (!req.nextauth.token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
+
+// Specify which routes this middleware should run on
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - login (login page)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|login).*)",
+  ],
+};
