86e759a3368e7bde6ec2e4e4f6ca46ffc0673f50
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2024-09-23 22:12:35 +0700

    feat: update modules import

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ConcertsModule } from './concerts/concerts.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    ConcertsModule,
+    ReservationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
